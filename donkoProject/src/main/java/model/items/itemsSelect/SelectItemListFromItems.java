139591588fb70e9c53693e53af3bc6a979b07153
N3question <narumi.uchiyama01@gmail.com>
2023-12-08 15:47:11 +0900

    ItemClassの商品の一覧を取得するメソッドの記述途中

--- donkoProject/src/main/java/model/items/itemsSelect/SelectItemListFromItems.java
+++ donkoProject/src/main/java/model/items/itemsSelect/SelectItemListFromItems.java
@@ -13,23 +13,28 @@ public class SelectItemListFromItems {
 	
 	//商品の一覧を取得する
 	public static ArrayList<ItemBean> selectItemListFromItems(){
-		final String SELECT_ITEMLIST_SQL = "SELECT * FROM human_resources_infos";
-		ArrayList<ItemBean> itemlist = new ArrayList<>();
+		final String SELECT_ITEMLIST_SQL = "SELECT * FROM items";
+		ArrayList<ItemBean> Itemlist = new ArrayList<>();
 		ArrayList<Object> paramLists = new ArrayList<Object>() {{ }};
 		try (Connection conn = DatabaseConnection.getConnection()) {
 			ResultSet result = GeneralDao.executeQuery(conn, SELECT_ITEMLIST_SQL, paramLists);
 			
 			while (result.next()) {
-				// TODO 以下変更する
+				ItemBean IBeans = new ItemBean();
+				int itemId = result.getInt("item_id");
+                String itemName = result.getString("item_name");
+                String imageFileName = result.getString("file_name");
                 
-                ItemBean IBeans = new ItemBean();
-                itemlist.add(IBeans);
+                IBeans.setItemId(itemId);
+                IBeans.setItemName(itemName);
+                IBeans.setImageFileName(imageFileName);
                 
+                Itemlist.add(IBeans);
             }
 		} catch (SQLException | ClassNotFoundException ex) {
 			ex.printStackTrace();
 		}
-		return itemlist;
+		return Itemlist;
 	};
 
 }
