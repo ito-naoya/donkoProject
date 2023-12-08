1c3084269998c5e16a67f398d6c047befe5c9b16
NakaharaErika <148533889+NakaharaErika@users.noreply.github.com>
2023-12-09 02:30:21 +0900

    重複を排除して商品名を取得

--- donkoProject/src/main/java/model/items/itemsSelect/SelectItemListWithoutDuplicate.java
+++ donkoProject/src/main/java/model/items/itemsSelect/SelectItemListWithoutDuplicate.java
@@ -1,13 +1,59 @@
 package model.items.itemsSelect;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import bean.ItemBean;
+import dao.DatabaseConnection;
+import dao.GeneralDao;
 
 public class SelectItemListWithoutDuplicate {
 	
 	//登録されている商品名を重複なしで取得する
 	public static ArrayList<ItemBean> selectItemListWithoutDuplicate(){
-		return null;};
+		
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("SELECT DISTINCT "		 	);
+		sb.append(	"file_name "					);
+		sb.append("FROM "	  					);
+		sb.append(	"items;"					);
+		
+		String sql = sb.toString();
+		
+		
+		List<Object> params = Arrays.asList();
+		try (Connection conn = DatabaseConnection.getConnection()){
+
+		    try(ResultSet rs = GeneralDao.executeQuery(conn, sql, params)) {
+
+		   		ArrayList<ItemBean> items = new ArrayList<>();
+		   		//OptionCategoryBeanに挿入
+		   		while(rs.next()) {
+		   			ItemBean item = new ItemBean();
+		        	String file = rs.getString("file_name");
+		        	item.setImageFileName(file);
+		        	
+		        	items.add(item);
+		        }
+			       //カテゴリーで紐づいたオプションのリストを返す
+			        return items;
+
+		    } catch (SQLException e) {
+		    	if(!conn.isClosed()) {
+			        conn.rollback();
+			        e.printStackTrace();
+		    	}
+		    }
+
+		} catch (SQLException | ClassNotFoundException e) {
+		  e.printStackTrace();
+		}
+		return null;
+	};
 
 }
