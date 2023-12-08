0857b0cb2cb6d1a2c0c8a294fad2a51c582b4daf
Minori-K15 <kutsukake.minori@geechs.com>
2023-12-08 13:42:43 +0900

    アノテーション追加
    user関連のアノテーションを追加しました

--- donkoProject/src/main/java/controller/user/UserSignupServlet.java
+++ donkoProject/src/main/java/controller/user/UserSignupServlet.java
@@ -1,15 +1,17 @@
 package controller.user;
 
+import java.io.IOException;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * Servlet implementation class UserSignupServlet
  */
+@WebServlet("/userSignup")
 public class UserSignupServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
