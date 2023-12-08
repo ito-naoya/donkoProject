6dafff534a5d18b81a27be9aab66a5e4dbaf578c
N3question <narumi.uchiyama01@gmail.com>
2023-12-08 13:39:56 +0900

    アノテーションの追加完了

--- donkoProject/src/main/java/controller/customer/ItemDetailServlet.java
+++ donkoProject/src/main/java/controller/customer/ItemDetailServlet.java
@@ -1,15 +1,14 @@
 package controller.customer;
 
+import java.io.IOException;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-/**
- * Servlet implementation class ItemDetailServlet
- */
+@WebServlet("/itemDetail")
 public class ItemDetailServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
