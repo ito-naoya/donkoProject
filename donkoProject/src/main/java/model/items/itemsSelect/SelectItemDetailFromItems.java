4511a3a0d9e58937d6a4a7d73b88326ef8cc6120
ito-naoya <148533254+ito-naoya@users.noreply.github.com>
2023-12-08 21:52:36 +0900

    一時保存

--- donkoProject/src/main/java/model/items/itemsSelect/SelectItemDetailFromItems.java
+++ donkoProject/src/main/java/model/items/itemsSelect/SelectItemDetailFromItems.java
@@ -37,6 +37,41 @@ public class SelectItemDetailFromItems {
 		sb2.append("	item_id != ?  ");
 		final String SELECT_FILENAME = sb2.toString();
 
+		StringBuilder selectDetailSql = new StringBuilder();
+		selectDetailSql.append("SELECT 			  ");
+		selectDetailSql.append("	*	 		  ");
+		selectDetailSql.append("FROM 			  ");
+		selectDetailSql.append("	items 		  ");
+		selectDetailSql.append("WHERE			  ");
+		selectDetailSql.append("	item_id = ?	  ");
+		final String SELECT_ITEMDETAIL_SQL = selectDetailSql.toString();
+
+		//商品の登録されている画像を全て取得
+		StringBuilder selectFileNameSql = new StringBuilder();
+		selectFileNameSql.append("SELECT 		  	   ");
+		selectFileNameSql.append("DISTINCT	  	   ");
+		selectFileNameSql.append("	file_name 	   ");
+		selectFileNameSql.append("FROM 		  	   ");
+		selectFileNameSql.append("	items 	  	   ");
+		selectFileNameSql.append("WHERE 		  	   ");
+		selectFileNameSql.append("	file_name != ? ");
+		selectFileNameSql.append("AND			  	   ");
+		selectFileNameSql.append("	item_name = ?  ");
+		final String SELECT_FILENAME_SQL = selectFileNameSql.toString();
+		
+		StringBuilder selectSizeSql = new StringBuilder();
+		selectSizeSql.append("SELECT ");
+		selectSizeSql.append("");
+		selectSizeSql.append();
+		selectSizeSql.append();
+		selectSizeSql.append();
+		selectSizeSql.append();
+		selectSizeSql.append();
+		selectSizeSql.append();
+		final String SELECT_SIZE_SQL selectSizeSql.toString();
+		
+		
+
 		ArrayList<Object> params = new ArrayList<Object>();
 		params.add(itemBean.getItemId());
 
@@ -48,15 +83,16 @@ public class SelectItemDetailFromItems {
 
 				conn.commit();
 				
-			} catch (SQLException e) {
-				if (conn != null && !conn.isClosed()) {
+			}catch (SQLException e) {
+				if (!conn.isClosed()) {
+					//SQL文が一つでも失敗したらロールバックする
 					conn.rollback();
 					e.printStackTrace();
 				}
 			}
+
 		} catch (SQLException |ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-
-	}
+}
 }
