47283960539f07906f792ce63bcc2648ff7b7891
N3question <narumi.uchiyama01@gmail.com>
2023-12-08 14:02:41 +0900

    なおぴ確認してくれ

--- donkoProject/src/main/java/controller/customer/ItemDetailServlet.java
+++ donkoProject/src/main/java/controller/customer/ItemDetailServlet.java
@@ -11,26 +11,15 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/itemDetail")
 public class ItemDetailServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+      
     public ItemDetailServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
--- donkoProject/src/main/java/controller/customer/PurchaseConfirmServlet.java
+++ donkoProject/src/main/java/controller/customer/PurchaseConfirmServlet.java
@@ -14,16 +14,13 @@ public class PurchaseConfirmServlet extends HttpServlet {
     
     public PurchaseConfirmServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
