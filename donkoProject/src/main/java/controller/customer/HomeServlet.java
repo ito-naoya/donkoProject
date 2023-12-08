abe0d183893df9e648de5f742800e17efe5f8fb1
N3question <narumi.uchiyama01@gmail.com>
2023-12-08 20:44:17 +0900

    ランダムで商品一覧の表示まで完了。画像の表示は未完了

--- donkoProject/src/main/java/controller/customer/HomeServlet.java
+++ donkoProject/src/main/java/controller/customer/HomeServlet.java
@@ -1,7 +1,11 @@
 package controller.customer;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
 
+import bean.ItemBean;
+import classes.Item;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -20,8 +24,18 @@ public class HomeServlet extends HttpServlet {
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String view = "/WEB-INF/views/customer/home.jsp";
-        request.getRequestDispatcher(view).forward(request, response);
+		ArrayList<ItemBean> itemList = Item.getItemList();
+		ArrayList<ItemBean> resultList = new ArrayList<>();
+		Random random = new Random();
+
+		for (int i = 0; i < 9 ; i++) {
+		    if (!itemList.isEmpty()) {
+		        int index = random.nextInt(itemList.size()); // ランダムなインデックスを生成
+		        ItemBean randomItem = itemList.get(index);
+		        resultList.add(randomItem);
+		    }
+		}
+		request.setAttribute("resultList", resultList);
 	}
 
 
--- donkoProject/src/main/java/model/items/itemsSelect/SelectItemListFromItems.java
+++ donkoProject/src/main/java/model/items/itemsSelect/SelectItemListFromItems.java
@@ -13,26 +13,38 @@ public class SelectItemListFromItems {
 	
 	//商品の一覧を取得する
 	public static ArrayList<ItemBean> selectItemListFromItems(){
-		final String SELECT_ITEMLIST_SQL = "SELECT * FROM items";
+		final String SELECT_ITEMLIST_SQL = "SELECT item_id, file_name, item_category_name, item_name FROM items";
 		ArrayList<ItemBean> Itemlist = new ArrayList<>();
-		ArrayList<Object> paramLists = new ArrayList<Object>() {{ }};
+		ArrayList<Object> paramLists = new ArrayList<>() {{ }};
 		try (Connection conn = DatabaseConnection.getConnection()) {
-			ResultSet result = GeneralDao.executeQuery(conn, SELECT_ITEMLIST_SQL, paramLists);
-			
-			while (result.next()) {
-				ItemBean IBeans = new ItemBean();
-				int itemId = result.getInt("item_id");
-                String itemName = result.getString("item_name");
-                String imageFileName = result.getString("file_name");
-                
-                IBeans.setItemId(itemId);
-                IBeans.setItemName(itemName);
-                IBeans.setImageFileName(imageFileName);
-                
-                Itemlist.add(IBeans);
-            }
-		} catch (SQLException | ClassNotFoundException ex) {
-			ex.printStackTrace();
+			try {
+				// resultsetはcloseの必要がない？ので無闇にtryの(）内に記述してcloseする必要はないかも
+				ResultSet result = GeneralDao.executeQuery(conn, SELECT_ITEMLIST_SQL, paramLists);
+				while (result.next()) {
+					ItemBean IBeans = new ItemBean();
+
+					int itemId = result.getInt("item_id");
+					String imageFileName = result.getString("file_name");
+					String itemCategoryName = result.getString("item_category_name");
+					String itemName = result.getString("item_name");
+
+					IBeans.setItemId(itemId);
+					IBeans.setItemName(itemName);
+					IBeans.setItemCategoryName(itemCategoryName);
+					IBeans.setImageFileName(imageFileName);
+
+					Itemlist.add(IBeans);
+				}
+				// SQL実行時のExceptionをcatch
+			} catch (SQLException e) {
+				if (conn != null && !conn.isClosed()&& !conn.getAutoCommit()) {
+					conn.rollback();
+					e.printStackTrace();
+				}
+			}
+			// getConnection時のExceptionをcatch
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
 		}
 		return Itemlist;
 	};
