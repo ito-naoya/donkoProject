83a36f74634cbbe1c7195df7898534e932410f14
ito-naoya <148533254+ito-naoya@users.noreply.github.com>
2023-12-08 12:36:17 +0900

    DeleteUserInfoServletのアノテーション修正
    deleteUserInfo　→　deleteUserInfoIndexに変更

--- donkoProject/src/main/java/controller/admin/DeleteUserInfoServlet.java
+++ donkoProject/src/main/java/controller/admin/DeleteUserInfoServlet.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/deleteUserInfo")
+@WebServlet("/deleteUserInfoIndex")
 public class DeleteUserInfoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
