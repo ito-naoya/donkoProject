fd1013aa03db9eb056f35953e67b285fa98498f9
N3question <narumi.uchiyama01@gmail.com>
2023-12-08 15:16:26 +0900

    トランザクションなしの記述

--- donkoProject/src/main/java/model/items/itemsSelect/SelectItemListFromItems.java
+++ donkoProject/src/main/java/model/items/itemsSelect/SelectItemListFromItems.java
@@ -1,13 +1,35 @@
 package model.items.itemsSelect;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import bean.ItemBean;
+import dao.DatabaseConnection;
+import dao.GeneralDao;
 
 public class SelectItemListFromItems {
 	
 	//商品の一覧を取得する
 	public static ArrayList<ItemBean> selectItemListFromItems(){
-		return null;};
+		final String SELECT_ITEMLIST_SQL = "SELECT * FROM human_resources_infos";
+		ArrayList<ItemBean> itemlist = new ArrayList<>();
+		ArrayList<Object> paramLists = new ArrayList<Object>() {{ }};
+		try (Connection conn = DatabaseConnection.getConnection()) {
+			ResultSet result = GeneralDao.executeQuery(conn, SELECT_ITEMLIST_SQL, paramLists);
+			
+			while (result.next()) {
+				// TODO 以下変更する
+                
+                ItemBean IBeans = new ItemBean();
+                itemlist.add(IBeans);
+                
+            }
+		} catch (SQLException | ClassNotFoundException ex) {
+			ex.printStackTrace();
+		}
+		return itemlist;
+	};
 
 }
