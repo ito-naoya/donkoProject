f200bd5efcfcb758875e5c7201aba9acf15cf39f
N3question <narumi.uchiyama01@gmail.com>
2023-12-09 16:32:02 +0900

    作業スタート

--- donkoProject/src/main/java/model/items/itemsSelect/SelectItemListFromItemsByCategory.java
+++ donkoProject/src/main/java/model/items/itemsSelect/SelectItemListFromItemsByCategory.java
@@ -8,6 +8,6 @@ public class SelectItemListFromItemsByCategory {
 	
 	//商品をカテゴリ指定して取得する
 	public static ArrayList<ItemBean> selectItemListFromItemsByCategory(ItemBean itemBean){
+		// カテゴリー一覧の表示を行う。
 		return null;};
-
 }
