29d7909d2ef7e682e0e5e004a255eec01acf1339
ito-naoya <148533254+ito-naoya@users.noreply.github.com>
2023-12-10 12:38:53 +0900

    表示画像切り替え機能完了

---
+++ donkoProject/src/main/webapp/js/itemDetailScript.js
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+ const mainImage = document.querySelector("#mainImage");
+ const mainSrc = mainImage.getAttribute("src");
+ const subImages = document.querySelectorAll(".subImage");
+
+subImages.forEach(img => {
+	
+	img.addEventListener("mouseover", () => {
+		const subSrc = img.getAttribute("src");
+		mainImage.setAttribute("src", subSrc);
+		img.classList.toggle("subImageOpacity");
+	})
+	
+	img.addEventListener("mouseout", () => {
+		mainImage.setAttribute("src", mainSrc);
+		img.classList.toggle("subImageOpacity");
+	})
+	
+})
+ 
+ 
+ 
