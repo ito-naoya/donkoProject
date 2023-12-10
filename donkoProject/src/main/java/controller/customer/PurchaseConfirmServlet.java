7fc2cff55d4b56629868c0e96310eb68a96f5976
ito-naoya <148533254+ito-naoya@users.noreply.github.com>
2023-12-10 20:40:21 +0900

    購入確認画面　一時保存

--- donkoProject/src/main/java/controller/customer/PurchaseConfirmServlet.java
+++ donkoProject/src/main/java/controller/customer/PurchaseConfirmServlet.java
@@ -1,12 +1,19 @@
 package controller.customer;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
+import bean.CartBean;
+import bean.ShippingAddressBean;
+import classes.Cart;
+import classes.ShippingAddress;
+import classes.user.CustomerUser;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/purchaseConfirm")
 public class PurchaseConfirmServlet extends HttpServlet {
@@ -17,7 +24,20 @@ public class PurchaseConfirmServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		HttpSession session = request.getSession();
+		CustomerUser loginedUser = (CustomerUser)session.getAttribute("customerUser");
+		
+		if(loginedUser == null) {
+			response.sendRedirect("login");
+			return;
+		}
+		
+		ShippingAddressBean shippingAddress = ShippingAddress.getDefaultShippingAddress(loginedUser);
+		ArrayList<CartBean> cartBeanList = Cart.getItemListFromCart(loginedUser);
+		
+		String view = "/WEB-INF/views/customer/purchaseConfirm.jsp";
+		request.getRequestDispatcher(view).forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
