e4d3993c9cba2fb3d26b091fae36e6e984494c4e
ito-naoya <148533254+ito-naoya@users.noreply.github.com>
2023-12-11 10:00:35 +0900

    購入確認画面　一時保存

--- donkoProject/src/main/java/controller/customer/PurchaseConfirmServlet.java
+++ donkoProject/src/main/java/controller/customer/PurchaseConfirmServlet.java
@@ -1,19 +1,12 @@
 package controller.customer;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
-import bean.CartBean;
-import bean.ShippingAddressBean;
-import classes.Cart;
-import classes.ShippingAddress;
-import classes.user.CustomerUser;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/purchaseConfirm")
 public class PurchaseConfirmServlet extends HttpServlet {
@@ -25,16 +18,19 @@ public class PurchaseConfirmServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		HttpSession session = request.getSession();
-		CustomerUser loginedUser = (CustomerUser)session.getAttribute("customerUser");
-		
-		if(loginedUser == null) {
-			response.sendRedirect("login");
-			return;
-		}
-		
-		ShippingAddressBean shippingAddress = ShippingAddress.getDefaultShippingAddress(loginedUser);
-		ArrayList<CartBean> cartBeanList = Cart.getItemListFromCart(loginedUser);
+//		HttpSession session = request.getSession();
+//		CustomerUser loginedUser = (CustomerUser)session.getAttribute("customerUser");
+//		
+//		if(loginedUser == null) {
+//			response.sendRedirect("login");
+//			return;
+//		}
+//		
+//		ShippingAddressBean shippingAddress = ShippingAddress.getDefaultShippingAddress(loginedUser);
+//		ArrayList<CartBean> cartBeanList = Cart.getItemListFromCart(loginedUser);
+//		
+//		request.setAttribute("shippingAddress", shippingAddress);
+//		request.setAttribute("cartBeanList", cartBeanList);
 		
 		String view = "/WEB-INF/views/customer/purchaseConfirm.jsp";
 		request.getRequestDispatcher(view).forward(request, response);
--- donkoProject/src/main/webapp/WEB-INF/views/customer/purchaseConfirm.jsp
+++ donkoProject/src/main/webapp/WEB-INF/views/customer/purchaseConfirm.jsp
@@ -1,12 +1,65 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>donko</title>
+<link
+	href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+	rel="stylesheet"
+	integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+	crossorigin="anonymous">
 </head>
 <body>
-
+	<main>
+		<a href="#" class="m-5" style="text-decoration: none; display: inline-block">
+			<h6 class="py-2 px-5"style="color: #385a37; border: 1px solid #385a37; text-align: center; width: 200px;">
+				DONKO
+			</h6>
+		</a>
+		<div class="container">
+			<div class="row">
+				<div class="col-8">
+					<p>
+						1. 配送先
+					</p>
+					<hr>
+					<div class="p-3">
+						<p class="mb-0">
+						〒000-000
+						</p>
+						<p>
+						○○○○○○○○○○x-x-x
+						</p>
+						<p>
+						○○○○○
+						</p>
+						<div class="d-flex justify-content-end">
+							<a href="#">
+								デフォルトの配送先を変更
+							</a>
+						</div>
+					</div>
+					<p>
+						1. 商品情報
+					</p>
+					<hr>
+				</div>
+				<div class="col-4">
+					<div class="d-flex flex-column border p-5 align-items-center justify-content-center" style="width: 280px; height: 200px;">
+						<p>
+						合計金額 ¥ 60,000
+						</p>
+						<form action="" method="post">
+							<button type=submit class="btn px-5 py-2" style="background-color: #9933ff; color: white;">
+								注文確定
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
 </body>
 </html>
