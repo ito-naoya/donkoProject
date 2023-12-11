ad88479c77d455a11093406879d73e165ae3942f
N3question <narumi.uchiyama01@gmail.com>
2023-12-11 16:55:44 +0900

    大まかな実装は完了。画像の取得について修正の必要あり。

--- donkoProject/src/main/java/model/items/itemsSelect/SelectItemListFromItems.java
+++ donkoProject/src/main/java/model/items/itemsSelect/SelectItemListFromItems.java
@@ -13,26 +13,44 @@ public class SelectItemListFromItems {
 	
 	//商品の一覧を取得する
 	public static ArrayList<ItemBean> selectItemListFromItems(){
-		final String SELECT_ITEMLIST_SQL = "SELECT item_id, file_name FROM items";
+		final String SELECT_ALL_ITEMLIST_SQL = "SELECT item_id, file_name FROM items WHERE items.item_delete_flg = 0 ORDER BY RAND()";
 		ArrayList<ItemBean> Itemlist = new ArrayList<>();
 		ArrayList<Object> paramLists = new ArrayList<>() {{ }};
 		try (Connection conn = DatabaseConnection.getConnection()) {
-			try (ResultSet result = GeneralDao.executeQuery(conn, SELECT_ITEMLIST_SQL, paramLists)) {
-				while (result.next()) {
-					ItemBean IBeans = new ItemBean();
-					
+			try (ResultSet result = GeneralDao.executeQuery(conn, SELECT_ALL_ITEMLIST_SQL, paramLists)) {
+				ItemBean IBeans = new ItemBean();
+				if(result.next()){ 
+					int itemId = result.getInt("item_id");
 					String imageFileName = result.getString("file_name");
-					
+					IBeans.setItemId(itemId);
 					IBeans.setImageFileName(imageFileName);
-
 					Itemlist.add(IBeans);
 				}
-				// SQL実行時のExceptionをcatch
+				String previousFN = IBeans.getImageFileName();
+				while (result.next()) {
+				    int itemId = result.getInt("item_id");
+				    String imageFileName = result.getString("file_name");
+
+				    if (!imageFileName.equals(previousFN)) {
+				        ItemBean IB = new ItemBean();
+				        IB.setItemId(itemId);
+				        IB.setImageFileName(imageFileName);
+				        Itemlist.add(IB);
+				        
+				        previousFN = imageFileName;
+				    }
+				    
+				    if (Itemlist.size() == 8) {
+				    	break;
+				    }
+				}
+			// SQL実行時のExceptionをcatch
 			} catch (SQLException e) {
 				if (conn != null && !conn.isClosed()&& !conn.getAutoCommit()) {
 					conn.rollback();
 					e.printStackTrace();
 				}
+				return null;
 			}
 			// getConnection時のExceptionをcatch
 		} catch (ClassNotFoundException | SQLException e) {
