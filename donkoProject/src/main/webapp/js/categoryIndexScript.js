d3c2d242da86afa2f775cf1142dd2a55760723f2
N3question <narumi.uchiyama01@gmail.com>
2023-12-12 18:54:56 +0900

    色変更

--- donkoProject/src/main/webapp/js/categoryIndexScript.js
+++ donkoProject/src/main/webapp/js/categoryIndexScript.js
@@ -3,6 +3,6 @@ function chebg(chkID){
     if(optionId.checked == true){
         optionId.parentNode.style.color = '#9933FF';
     }else{
-        optionId.parentNode.style.color = '#E5CCFF';
+        optionId.parentNode.style.color = '#000';
     }
 }
