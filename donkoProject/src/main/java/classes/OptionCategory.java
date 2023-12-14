7c8a7f5c30c5fb9a4f7dc8c6e88c5af0361a8cc2
N3question <narumi.uchiyama01@gmail.com>
2023-12-14 17:45:39 +0900

    コメントの追加

--- donkoProject/src/main/java/classes/OptionCategory.java
+++ donkoProject/src/main/java/classes/OptionCategory.java
@@ -23,12 +23,16 @@ public class OptionCategory {
 	//オプションのカテゴリ名で全てのオプションを取得する
 	public static ArrayList<ArrayList<OptionCategoryBean>> getOptionCategoryListAllByCategory(ItemBean itemBean) {
 		ArrayList<ArrayList<OptionCategoryBean>> optionCategoryValueListAll = new ArrayList<ArrayList<OptionCategoryBean>>();
+		
 		// カテゴリー名を指定 : 付随した全てのオプションカテゴリー名を取得
+		// 衣類 : 衣類サイズ, 色
 		ArrayList<ItemCategoryBean> optionCategoryNameList = ItemCategory.getItemOptionCategoryNameListByCategory(itemBean);
 		
-		// オプションカテゴリー名を指定 : 付随した全てのオプションカテゴリー値を取得
 		for (int i = 0; i < optionCategoryNameList.size(); i++) {
+			// i番目のオプションカテゴリー名の取得
 			ItemCategoryBean optionCategoryName = optionCategoryNameList.get(i);
+			// オプションカテゴリー名を指定 : 付随した全てのオプションカテゴリー値を取得
+			// 衣類サイズ : S, M, L
 			ArrayList<OptionCategoryBean> optionCategoryValueList = getOptionCategoryListByCategory(optionCategoryName);
 			optionCategoryValueListAll.add(optionCategoryValueList);
 		}
