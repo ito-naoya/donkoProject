0fea7f910bfc11f468293d844b71c6426a9e553b
N3question <narumi.uchiyama01@gmail.com>
2023-12-14 19:04:27 +0900

    string bilderを使用するのと、リファクタリング

--- donkoProject/src/main/java/model/items/itemsSelect/SelectItemListFromItemsByOption.java
+++ donkoProject/src/main/java/model/items/itemsSelect/SelectItemListFromItemsByOption.java
@@ -12,14 +12,31 @@ import dao.GeneralDao;
 public class SelectItemListFromItemsByOption {
 	//商品をカテゴリとオプション指定して取得する
 	public static ArrayList<ItemBean> selectItemListFromItemsByOption(String[] checkedOption, String categoryName){
-		final String SELECT_OPTION_ITEMLIST_SQL = "SELECT items.item_id, items.item_name, items.file_name "
-				+ "FROM items "
-				+ "INNER JOIN options ON items.item_id = options.item_id "
-				+ "INNER JOIN option_categories ON "
-				+ "options.option_category_name = option_categories.option_category_name AND "
-				+ "options.option_category_increment_id = option_categories.option_category_increment_id "
-				+ "WHERE items.item_category_name = ? "
-				+ "AND option_categories.option_category_value IN (" + questionNum(checkedOption) + ")";
+		StringBuilder sb = new StringBuilder();
+		sb.append("SELECT ");
+		sb.append("    items.item_id, ");
+		sb.append("    items.item_name, ");
+		sb.append("    items.file_name ");
+		sb.append("FROM ");
+		sb.append("    items ");
+		sb.append("INNER JOIN ");
+		sb.append("    options ");
+		sb.append("ON ");
+		sb.append("    items.item_id = options.item_id ");
+		sb.append("INNER JOIN ");
+		sb.append("    option_categories ");
+		sb.append("ON ");
+		sb.append("    options.option_category_name = option_categories.option_category_name ");
+		sb.append("AND ");
+		sb.append("    options.option_category_increment_id = option_categories.option_category_increment_id ");
+		sb.append("WHERE ");
+		sb.append("    items.item_category_name = ? ");
+		sb.append("AND ");
+		sb.append("    option_categories.option_category_value ");
+		sb.append("    IN (" + questionNum(checkedOption) + ")");
+		
+		// SQLを文字列化
+		final String SELECT_OPTION_ITEMLIST_SQL = sb.toString();
 
 		ArrayList<Object> paramList = new ArrayList<>() {{
 			add(categoryName);
@@ -31,19 +48,6 @@ public class SelectItemListFromItemsByOption {
 		try (Connection conn = DatabaseConnection.getConnection()) {
 			try (ResultSet result = GeneralDao.executeQuery(conn, SELECT_OPTION_ITEMLIST_SQL, paramList)) {
 				
-				// 1回目
-				if(result.next()){ 
-					ItemBean itemBean = new ItemBean();
-					int itemId = result.getInt("item_id");
-					String itemName = result.getString("item_name");
-					String imageFileName = result.getString("file_name");
-					itemBean.setItemId(itemId);
-					itemBean.setItemName(itemName);
-					itemBean.setImageFileName(imageFileName);
-					optionList.add(itemBean);
-				}
-				
-				// 2回目以降
 				while(result.next()){ 
 					ItemBean IBeans = new ItemBean();
 					int itemId = result.getInt("item_id");
@@ -51,9 +55,9 @@ public class SelectItemListFromItemsByOption {
 					String imageFileName = result.getString("file_name");
 					
 					// 画像名が一致するかを判定
-					boolean isNotExist = isNotExist(optionList, imageFileName);
+					boolean isNotExist = checkExist(optionList, imageFileName);
 					// 一致しない場合は配列に格納
-					if (isNotExist == true) {
+					if (isNotExist) {
 						IBeans.setItemId(itemId);
 						IBeans.setItemName(itemName);
 						IBeans.setImageFileName(imageFileName);
@@ -84,22 +88,20 @@ public class SelectItemListFromItemsByOption {
 	    return str;
 	}
 	
-	// 取得したデータを格納する配列の中に同じ画像名がないかを判定
-	private static boolean isNotExist(ArrayList<ItemBean> optionList, String imageFileName) {
-		boolean isNotExist = false;
-		for (int i = 0; i < optionList.size(); i++) {
-			// i番目のファイル名の取得
-			ItemBean itembean = optionList.get(i);
-			String fileNameInList = itembean.getImageFileName();
-			
-			// ファイル名が一致するかを判定
-			if (!imageFileName.equals(fileNameInList)) {
-				isNotExist = true;
-			} else {
-				isNotExist = false;
-				break;
+	// 取得したデータを格納した配列内に同じ画像名が存在するかを判定
+		private static boolean checkExist(ArrayList<ItemBean> optionList, String imageFileName) {
+			boolean isNotExist = true;
+			for (int i = 0; i < optionList.size(); i++) {
+				// i番目のファイル名の取得
+				ItemBean itemBean = optionList.get(i);
+				String fileNameInList = itemBean.getImageFileName();
+				
+				// 配列の中のファイル名が同じかを判定
+				if (imageFileName.equals(fileNameInList)) {
+					isNotExist = false;
+					break;
+				}
 			}
+			return isNotExist;
 		}
-		return isNotExist;
-	}
 }
--- donkoProject/src/main/java/model/items/itemsSelect/SelectRandomItemListFromItems.java
+++ donkoProject/src/main/java/model/items/itemsSelect/SelectRandomItemListFromItems.java
@@ -13,31 +13,34 @@ public class SelectRandomItemListFromItems {
 	
 	//商品の一覧を取得する
 	public static ArrayList<ItemBean> selectItemListFromItems(){
-		final String SELECT_ALL_ITEMLIST_SQL = "SELECT item_id, file_name FROM items WHERE items.item_delete_flg = 0 ORDER BY RAND()";
+		StringBuilder sb = new StringBuilder();
+		sb.append("SELECT ");
+		sb.append("  items.item_id, ");
+		sb.append("  items.file_name ");
+		sb.append("FROM ");
+		sb.append("  items ");
+		sb.append("WHERE ");
+		sb.append("  items.item_delete_flg = 0 ");
+		sb.append("ORDER BY ");
+		sb.append("  RAND()");
+		
+		//SQLを文字列化
+		final String SELECT_ALL_ITEMLIST_SQL = sb.toString();
+		
 		ArrayList<ItemBean> itemlist = new ArrayList<>();
 		ArrayList<Object> paramList = new ArrayList<>() {{ }};
 		try (Connection conn = DatabaseConnection.getConnection()) {
 			try (ResultSet result = GeneralDao.executeQuery(conn, SELECT_ALL_ITEMLIST_SQL, paramList)) {
-				// 1回目
-				if(result.next()){ 
-					ItemBean itemBean = new ItemBean();
-					int itemId = result.getInt("item_id");
-					String imageFileName = result.getString("file_name");
-					itemBean.setItemId(itemId);
-					itemBean.setImageFileName(imageFileName);
-					itemlist.add(itemBean);
-				}
 				
-				// 2回目以降
 				while (result.next()) {
 					ItemBean itemBean = new ItemBean();
 				    int itemId = result.getInt("item_id");
 				    String imageFileName = result.getString("file_name");
 				    
 				    // 画像の重複を判定
-				    boolean isNotExist = isNotExist(itemlist, imageFileName);
+				    boolean isNotExist = checkExist(itemlist, imageFileName);
 				    // 重複しないデータを配列に格納
-					if (isNotExist == true) {
+					if (isNotExist) {
 						itemBean.setItemId(itemId);;
 						itemBean.setImageFileName(imageFileName);
 						itemlist.add(itemBean);
@@ -65,17 +68,15 @@ public class SelectRandomItemListFromItems {
 	};
 	
 	// 取得したデータを格納した配列内に同じ画像名が存在するかを判定
-	private static boolean isNotExist(ArrayList<ItemBean> itemList, String imageFileName) {
-		boolean isNotExist = false;
+	private static boolean checkExist(ArrayList<ItemBean> itemList, String imageFileName) {
+		boolean isNotExist = true;
 		for (int i = 0; i < itemList.size(); i++) {
 			// i番目のファイル名の取得
 			ItemBean itemBean = itemList.get(i);
 			String fileNameInList = itemBean.getImageFileName();
 			
 			// 配列の中のファイル名が同じかを判定
-			if (!imageFileName.equals(fileNameInList)) {
-				isNotExist = true;
-			} else {
+			if (imageFileName.equals(fileNameInList)) {
 				isNotExist = false;
 				break;
 			}
