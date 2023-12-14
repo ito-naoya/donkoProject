b149bcd3c0d88b0c6142d0a778a82537ec9726ab
Minori-K15 <kutsukake.minori@geechs.com>
2023-12-14 16:32:34 +0900

    Userクラス修正
    セッションがないので、一旦修飾子をstaticに修正しました。
    セッションが設定でき次第戻します。

--- donkoProject/src/main/java/classes/user/CustomerUser.java
+++ donkoProject/src/main/java/classes/user/CustomerUser.java
@@ -8,57 +8,91 @@ public class CustomerUser extends User {
 	};
 
 	//ログインID
-	private String userLoginId;
+	private static String userLoginId;
 	//パスワード
-	private String password;
+	private static String password;
 	//ユーザー名
-	private String userName;
+	private static String userName;
 	//性別
-	private String gender;
+	private static String gender;
 	//生年月日
-	private Date birthday;
+	private static Date birthday;
 	//削除フラグ
 	private boolean isDeleted;
+/*
+ * TODO：あとで	戻す
+ * //ログインID
+ * 	private String userLoginId;
+ * 	//パスワード
+ * 	private String password;
+ * //ユーザー名
+ * 	private String userName;
+ * 	//性別
+ * 	private String gender;
+ * 	//生年月日
+ * 	private Date birthday;
+ * */
+
 
 	public String getUserLoginId() {
 		return userLoginId;
 	}
 
-	public void setUserLoginId(String userLoginId) {
-		this.userLoginId = userLoginId;
+	public static void setUserLoginId(String userLoginId) {
+		CustomerUser.userLoginId = userLoginId;
 	}
+//	TODO:あとで戻す
+//	public void setUserLoginId(String userLoginId) {
+//		this.userLoginId = userLoginId;
+//	}
 
 	public String getPassword() {
 		return password;
 	}
 
-	public void setPassword(String password) {
-		this.password = password;
+	public static void setPassword(String password) {
+		CustomerUser.password = password;
 	}
+//	TODO:あとで戻す
+//	public void setPassword(String password) {
+//		this.password = password;
+//	}
 
 	public String getUserName() {
 		return userName;
 	}
 
-	public void setUserName(String userName) {
-		this.userName = userName;
+	public static void setUserName(String userName) {
+		CustomerUser.userName = userName;
 	}
+//	TODO:あとで戻す
+//	public void setUserName(String userName) {
+//		this.userName = userName;
+//	}
 
 	public String getGender() {
 		return gender;
 	}
 
-	public void setGender(String gender) {
-		this.gender = gender;
+	public static void setGender(String gender) {
+		CustomerUser.gender = gender;
 	}
+//	TODO:あとで戻す
+//	public void setGender(String gender) {
+//		this.gender = gender;
+//	}
 
 	public Date getBirthday() {
 		return birthday;
 	}
 
-	public void setBirthday(Date birthday) {
-		this.birthday = birthday;
+	public static void setBirthday(Date birthday) {
+		CustomerUser.birthday = birthday;
 	}
+//	TODO:あとで戻す
+//	public void setBirthday(Date birthday) {
+//		this.birthday = birthday;
+//	}
 
 	public boolean isDeleted() {
 		return isDeleted;
