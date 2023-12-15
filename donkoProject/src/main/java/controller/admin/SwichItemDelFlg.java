92e0c570ff495a5e5816ffb0303d33181c0e750d
NakaharaErika <148533889+NakaharaErika@users.noreply.github.com>
2023-12-15 15:05:57 +0900

    一時保存

---
+++ donkoProject/src/main/java/controller/admin/SwichItemDelFlg.java
@@ -0,0 +1,37 @@
+package controller.admin;
+
+import java.io.IOException;
+
+import classes.Item;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/swichItemDelFlg")
+public class SwichItemDelFlg extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public SwichItemDelFlg() {
+		super();
+	}
+
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+//			throws ServletException, IOException {
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+//	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		//選択済みの商品ステータスを全て取得、配列に格納
+		String[] itemStatus = request.getParameterValues("itemStatus");
+
+		//選択済みの商品のステータスを切り替えるサーブレット
+		if(itemStatus != null && itemStatus.length > 0) {
+			Item.deleteItem(itemStatus);
+		}
+	}
+
+}
