b62489eecb8b1b8f51735bf3f65a23182ab1ed34
NakaharaErika <148533889+NakaharaErika@users.noreply.github.com>
2023-12-19 16:58:12 +0900

    Update DeleteItemIndexServlet.java

--- donkoProject/src/main/java/controller/admin/DeleteItemIndexServlet.java
+++ donkoProject/src/main/java/controller/admin/DeleteItemIndexServlet.java
@@ -26,8 +26,16 @@ public class DeleteItemIndexServlet extends HttpServlet {
 		//TODO:セッション管理
 		//ログイン画面実装後にセットします
 
-		//カテゴリー一覧を取得(戻り値がnullでもjspに渡す)
+		//カテゴリー一覧を取得
 		ArrayList<ItemCategoryBean> categoryList = ItemCategory.getItemCategoryList();
+		if(categoryList == null) {
+			//取得情報の不備があれば、エラー画面に遷移
+			request.setAttribute("errorMessage", "カテゴリー一覧の取得に失敗しました");
+			request.setAttribute("url","adminTopPage");
+			String view = "/WEB-INF/views/component/message.jsp";
+			request.getRequestDispatcher(view).forward(request, response);
+		}
+
 		//配列に「全ての商品」という項目を追加
 		ItemCategoryBean allItem = new ItemCategoryBean();
 		allItem.setItemCategoryName("全ての商品");
@@ -40,6 +48,13 @@ public class DeleteItemIndexServlet extends HttpServlet {
 		request.setAttribute("categoryList", categoryList);
 		//初期表示として、「すべてのカテゴリの商品を、削除済みのものも含めて全て表示する」(戻り値がnullでもjspに渡す)
 		ArrayList<ItemBean> itemList = Item.getItemAndOptionListAll("");
+		if(itemList == null) {
+			//取得情報の不備があれば、エラー画面に遷移
+			request.setAttribute("errorMessage", "商品一覧の取得に失敗しました");
+			request.setAttribute("url","adminTopPage");
+			String view = "/WEB-INF/views/component/message.jsp";
+			request.getRequestDispatcher(view).forward(request, response);
+		}
 		request.setAttribute("itemList", itemList);
 
 		//商品一覧画面に遷移
@@ -48,19 +63,26 @@ public class DeleteItemIndexServlet extends HttpServlet {
 
 	}
 
+	//ソートを実行
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		//TODO:セッション管理
 		//ログイン画面実装後にセットします
-		//ソートを実行
 
 		//カテゴリーを取得
 		String itemCategoryName = request.getParameter("itemCategoryName");
 		//表示方法を取得
 		Integer sortParam = Integer.parseInt(request.getParameter("itemDelFlg"));
 
-		//カテゴリー一覧を取得(戻り値がnullでもjspに渡す)
+		//カテゴリー一覧を取得
 		ArrayList<ItemCategoryBean> categoryList = ItemCategory.getItemCategoryList();
+		if(categoryList == null) {
+			//取得情報の不備があれば、エラー画面に遷移
+			request.setAttribute("errorMessage", "カテゴリー一覧の取得に失敗しました");
+			request.setAttribute("url","adminTopPage");
+			String view = "/WEB-INF/views/component/message.jsp";
+			request.getRequestDispatcher(view).forward(request, response);
+		}
 		//配列に「全ての商品」という項目を追加
 		ItemCategoryBean allItem = new ItemCategoryBean();
 		allItem.setItemCategoryName("全ての商品");
@@ -70,7 +92,7 @@ public class DeleteItemIndexServlet extends HttpServlet {
 		//カテゴリ一覧をセット
 		request.setAttribute("categoryList", categoryList);
 
-		//以下、ソートSQL(戻り値がnullでもjspに渡す)
+		//以下、ソートSQL
 		ArrayList<ItemBean> itemList = null;
 		if(!itemCategoryName.isEmpty() && sortParam == 2) { //sortParamが2なら、全商品を抽出
 			itemList = Item.getItemAndOptionListAll(itemCategoryName);
@@ -78,6 +100,15 @@ public class DeleteItemIndexServlet extends HttpServlet {
 		} else if(!itemCategoryName.isEmpty() && sortParam < 2 ) { //sortParamが2未満なら、削除フラグを確認して抽出
 			itemList = Item.getItemAndOptionListByDelFlg(sortParam, itemCategoryName);
 		}
+
+		if(itemList == null) {
+			//取得情報の不備があれば、エラー画面に遷移
+			request.setAttribute("errorMessage", "商品一覧の取得に失敗しました");
+			request.setAttribute("url","adminTopPage");
+			String view = "/WEB-INF/views/component/message.jsp";
+			request.getRequestDispatcher(view).forward(request, response);
+		}
+
 		request.setAttribute("itemList", itemList);
 		//指定された表示方法をセット
 		request.setAttribute("itemDelFlg", sortParam);
