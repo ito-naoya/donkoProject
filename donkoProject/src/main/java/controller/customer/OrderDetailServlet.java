eba12cadfc0e15f8a63b6a7a14f8f7f0da3c838a
Minori-K15 <kutsukake.minori@geechs.com>
2023-12-21 11:59:56 +0900

    購入詳細画面を修正
    購入詳細画面で配送先の情報と配送日、合計金額と商品単価を表示できるように修正しました。
    確認よろしくお願いします！

--- donkoProject/src/main/java/controller/customer/OrderDetailServlet.java
+++ donkoProject/src/main/java/controller/customer/OrderDetailServlet.java
@@ -3,13 +3,19 @@ package controller.customer;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import bean.PurchaseBean;
 import bean.PurchaseDetailBean;
+import bean.ShippingAddressBean;
+import classes.Purchase;
 import classes.PurchaseDetail;
+import classes.ShippingAddress;
+import classes.user.CustomerUser;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/orderDetail")
 public class OrderDetailServlet extends HttpServlet {
@@ -20,10 +26,41 @@ public class OrderDetailServlet extends HttpServlet {
 	}
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// ユーザーID取得のためにインスタンス化
+		CustomerUser customerUser = new CustomerUser();
+		PurchaseBean purchaseBean = new PurchaseBean();
+		ShippingAddressBean shippingAddressBean = new ShippingAddressBean();
+		
+		// セッション確認
+		HttpSession session = request.getSession(false);
+		if(session == null) {
+			response.sendRedirect("home");
+			return;
+		} else {
+			// ユーザーIDを取得してセット
+			customerUser.setUserId((int)session.getAttribute("user_id"));
+		}
 		
 		// 購入ID取得 
-		int purchase_id =  Integer.parseInt(request.getParameter("purchase_id"));
+		int purchase_id = Integer.parseInt(request.getParameter("purchase_id"));
 		request.setAttribute("purchase_id", purchase_id);
+		
+		// 購入IDをセット
+		purchaseBean.setPurchaseId(purchase_id);
+		
+		// 1件の購入情報を取得
+		PurchaseBean purchaseInfo = Purchase.getPurchaseInfo(purchaseBean);
+		request.setAttribute("purchaseInfo", purchaseInfo);
+		
+		// ShippingAddressBeanにセット
+		shippingAddressBean.setUserId(customerUser.getUserId());
+		shippingAddressBean.setShippingAddressId(purchaseInfo.getShippingAddressId());
+		
+		// 配送先詳細情報取得
+		ShippingAddressBean shippingAddressInfo = ShippingAddress.getShippingAddressDetail(shippingAddressBean);
+		request.setAttribute("shippingAddressInfo", shippingAddressInfo);
+		
+		// PurchaseDetailBeanにセット
 		PurchaseDetailBean purchaseDetailBean = new PurchaseDetailBean();
 		purchaseDetailBean.setPurchaseId(purchase_id);
 		
