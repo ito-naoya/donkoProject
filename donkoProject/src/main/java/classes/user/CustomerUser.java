b4ab9ddd658a55b10a60acf784b8b4586bb4c4d7
ito-naoya <148533254+ito-naoya@users.noreply.github.com>
2023-12-22 11:29:04 +0900

    バリデーション修正

--- donkoProject/src/main/java/classes/user/CustomerUser.java
+++ donkoProject/src/main/java/classes/user/CustomerUser.java
@@ -2,7 +2,11 @@ package classes.user;
 
 import java.util.Date;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
 import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
 
 public class CustomerUser extends User {
 
@@ -10,15 +14,21 @@ public class CustomerUser extends User {
 	};
 
 	//ログインID
-	@Length(min= 5 , max= 10 )
+	@NotNull(message= "入力必須項目です")
+	@Length(min= 5 , max= 10, message="{min}以上{max}以下で入力してください。" )
 	private String userLoginId;
 	//パスワード
+	@Range(min=8, max=8, message="８文字で入力してください。" ) 
+	@NotNull(message= "入力必須項目です")
 	private String password;
 	//ユーザー名
+	@NotNull(message= "入力必須項目です")
+	@Length(min=1, max= 25, message="{max}以内で入力してください。") 
 	private String userName;
 	//性別
 	private String gender;
 	//生年月日
+	@Past(message= "不正な値が入力されています。")
 	private Date birthday;
 	//削除フラグ
 	private boolean isDeleted;
