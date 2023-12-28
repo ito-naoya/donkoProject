1ae9bd7646448c7b7d0c61765d5e53f402efef91
NakaharaErika <148533889+NakaharaErika@users.noreply.github.com>
2023-12-28 21:55:27 +0900

    修正完了

--- donkoProject/src/main/webapp/js/deleteItemScript.js
+++ donkoProject/src/main/webapp/js/deleteItemScript.js
@@ -1,13 +1,19 @@
-function changeTextColor(checkbox, labelId) {
-    var label = document.getElementById(labelId);
-    if (checkbox.checked) {
-        // チェックボックスがチェックされたら、テキストの色を赤にする
-        label.style.color = 'red';
-    } else {
-        // チェックボックスがチェックされていなければ、削除済みなら灰色、それ以外なら緑色に戻す
-        label.style.color = label.innerText === '削除済み' ? '#CCC' : '#00FF00';
-    }
-}
+
 function confirmStatusChange() {
-    return confirm('ステータスを切り替えます。よろしいですか？');//アラートがうごかねぇ
+    return confirm('ステータスを切り替えます。よろしいですか？');
 }
+
+// チェックボックスのクリックイベントを取得して処理する関数
+function checkboxClicked(event) {
+	event.stopPropagation();
+ }
+
+// ページが読み込まれた後に実行
+window.onload = function() {
+   // チェックボックス要素をすべて取得
+   const checkboxes = document.querySelectorAll('input[type="checkbox"][name="itemStatus"]');
+   // 各チェックボックスに対してイベントリスナーを設定
+   checkboxes.forEach(function(checkbox) {
+       checkbox.addEventListener('click', checkboxClicked);
+   });
+};
