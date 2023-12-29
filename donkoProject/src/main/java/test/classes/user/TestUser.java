ab3172558fe7e17b9b1fabaaa1a4dec8a5e9a57f
Minori-K15 <kutsukake.minori@geechs.com>
2023-12-29 14:40:23 +0900

    パスワード変更のレイアウト変更
    あとでまとめてマージします

---
+++ donkoProject/src/main/java/test/classes/user/TestUser.java
@@ -0,0 +1,17 @@
+package test.classes.user;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import classes.user.CustomerUser;
+
+public class TestUser {
+	// 
+    CustomerUser customerUser;
+    @BeforeEach
+    void setUp() {
+    	customerUser = new CustomerUser();
+    }
+    
+    // 
+    
+}
