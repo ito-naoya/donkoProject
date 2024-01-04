7f0f3bcd3ba00274a0ebd82dcf035311bb09cbad
NakaharaErika <148533889+NakaharaErika@users.noreply.github.com>
2024-01-04 12:01:08 +0900

    オプション/カテゴリテストケース

--- donkoProject/src/main/java/test/model/options/optionsInsert/TestInsertItemOptionValue.java
+++ donkoProject/src/main/java/test/model/options/optionsInsert/TestInsertItemOptionValue.java
@@ -1,13 +1,13 @@
-package test.model.options.optionsDelete;
+package test.model.options.optionsInsert;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
 import bean.OptionCategoryBean;
-import model.options.optionsDelete.DeleteItemOptionValue;
+import model.options.optionsInsert.InsertItemOptionValue;
 
-class TestDeleteItemOptionValue {
+class TestInsertItemOptionValue {
 
 	//成功テスト
 	@Test
@@ -15,27 +15,27 @@ class TestDeleteItemOptionValue {
 		OptionCategoryBean option = new OptionCategoryBean();
 		option.setOptionCategoryName("hoge");
 		option.setOptionCategoryValue("fuga");
-		Integer result = DeleteItemOptionValue.deleteItemOptionValue(option);
+		Integer result = InsertItemOptionValue.insertItemOptionValue(option);
 		assertEquals(1,result);
 	}
 
-	//失敗テスト
+	//失敗テスト(値の重複)
 	@Test
 	void testException1() {
 		OptionCategoryBean option = new OptionCategoryBean();
 		option.setOptionCategoryName("衣類");
-		option.setOptionCategoryName("衣類サイズ");
-		Integer result = DeleteItemOptionValue.deleteItemOptionValue(option);
+		option.setOptionCategoryValue("衣類サイズ");
+		Integer result = InsertItemOptionValue.insertItemOptionValue(option);
 		assertEquals(0,result);
 	}
 
-//	//失敗テスト
+//	//失敗テスト(sb1のインジェクション)
 //	@Test
 //	void testException2() {
 //		OptionCategoryBean option = new OptionCategoryBean();
-//		option.setOptionCategoryName("hoge");
-//		option.setOptionCategoryValue("fuga");
-//		Integer result = DeleteItemOptionValue.deleteItemOptionValue(option);
+//		option.setOptionCategoryName("衣類");
+//		option.setOptionCategoryValue("衣類サイズ");
+//		Integer result = InsertItemOptionValue.insertItemOptionValue(option);
 //		assertEquals(null,result);
 //	}
 }
