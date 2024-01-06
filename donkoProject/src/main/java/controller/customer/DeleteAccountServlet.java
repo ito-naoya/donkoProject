7f75d0ae80317ac65cac7d693077e4ef41e0c7f1
N3question <narumi.uchiyama01@gmail.com>
2024-01-06 15:41:06 +0900

    セッション制限を追加

--- donkoProject/src/main/java/controller/customer/DeleteAccountServlet.java
+++ donkoProject/src/main/java/controller/customer/DeleteAccountServlet.java
@@ -2,11 +2,13 @@ package controller.customer;
 
 import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/deleteAccount")
 public class DeleteAccountServlet extends HttpServlet {
@@ -18,6 +20,19 @@ public class DeleteAccountServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// セッションの確認
+		HttpSession session = request.getSession(false);
+		if(session == null) {
+			// セッションがなければホーム画面に遷移
+			response.sendRedirect("home");
+			return;
+		}
+		
+		// ヘッダーの値を取得
+		String disp = "/header";
+	    RequestDispatcher dispatch = request.getRequestDispatcher(disp);
+	    dispatch.include(request, response);
+		
 		String view = "/WEB-INF/views/customer/deleteAccount.jsp";
         request.getRequestDispatcher(view).forward(request, response);
 	}
